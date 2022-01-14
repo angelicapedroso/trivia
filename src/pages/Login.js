36bce6c91d49a558b2968840a9ecc84adfdf0767
Itaji Carvalho <itaji.1999@gmail.com>
2022-01-14 12:05:32 -0300

    adiciona Req-1

---
+++ src/pages/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from '../trivia.png';
+import '../App.css';
+
+class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isDisabled: true,
+      user: '',
+      email: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    const { user, email } = this.state;
+    this.setState({ [name]: value });
+    if (user.length > 0 && email.length > 0) this.setState({ isDisabled: false });
+  }
+
+  render() {
+    const { isDisabled, user, email } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <label htmlFor="input-player-name">
+            <input
+              name="user"
+              value={ user }
+              onChange={ this.handleChange }
+              data-testid="input-player-name"
+            />
+          </label>
+          <label htmlFor="input-gravatar-email">
+            <input
+              name="email"
+              value={ email }
+              onChange={ this.handleChange }
+              data-testid="input-gravatar-email"
+            />
+          </label>
+          <button type="button" disabled={ isDisabled }>
+            Play
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Login;
