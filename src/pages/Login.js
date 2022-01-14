78d75b69d0bccf55db36d8df089d2906a8aa618d
Itaji Carvalho <itaji.1999@gmail.com>
2022-01-14 13:18:46 -0300

    adiciona data-testid ao button

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -41,7 +41,7 @@ class Login extends React.Component {
               data-testid="input-gravatar-email"
             />
           </label>
-          <button type="button" disabled={ isDisabled }>
+          <button data-testid="btn-play" type="button" disabled={ isDisabled }>
             Play
           </button>
         </header>
