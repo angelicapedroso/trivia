8d32029b341ce24f3d4d73f2b571cef70008cbf9
Itaji Carvalho <itaji.1999@gmail.com>
2022-01-14 13:58:47 -0300

    origin

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducer from '../reducers/index';
 
-const store = createStore(reducer, (applyMiddleware(thunk)));
+const store = createStore(reducer, applyMiddleware(thunk));
 
 if (window.Cypress) {
   window.store = store;
